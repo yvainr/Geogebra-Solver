64dbe7fd00dbf0f7b6e8208d1260579569055153
Yan <exemfortest@gmail.com>
2022-06-27 21:54:02 +0300

    geogebra drawer v0.9.2

--- drawer_ggb_0.0.1/geogebra_html_generator.py
+++ drawer_ggb_0.0.1/geogebra_html_generator.py
@@ -1,5 +1,6 @@
 import logging
 
+
 def insert_commands(list_of_commands: list, output_file_name='page_test.html'):
     """Принимает на вход список строк-команд geogebra, возвращает код html страницы, 
     отображающей чертёж, постороенный по этим командам и создаёт такой файл в той же дирректории, что и запущена"""
@@ -15,7 +16,8 @@ def insert_commands(list_of_commands: list, output_file_name='page_test.html'):
 
     html_page_file.close()
 
-    edited_code = html_code.replace('//////', js_commands_text) #пока что так, но потом нужно будет перейти на шаблоны jinja2
+    edited_code = html_code.replace('//////',
+                                    js_commands_text)  # пока что так, но потом нужно будет перейти на шаблоны jinja2
 
     output_html = open('page_test.html', 'w')
 
