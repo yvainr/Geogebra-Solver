faecdf78c32a1843d02d69af151fcf0782ca86fd
Euggenius <108299501+Euggenius@users.noreply.github.com>
2022-07-03 07:04:11 +0300

    Update task_parser.py
    Рекомендую всем ознакомится, потому что изменилось примерно вообще всё.

--- task_parser.py
+++ task_parser.py
@@ -1,25 +1,25 @@
 from fractions import Fraction
 # для удобной работы с отношениями
-from itertools import combinations
+# from itertools import combinations
+# для всех сочетаний элементов из списка
 from pprint import pprint
 from copy import deepcopy
 
 
 class Point:
-    def __init__(self, name, cords=[]):
+    def __init__(self, name):
         self.name = name
-        self.cords = cords
+        # лежит ли внутри многоугольника
 
 
 class Line:
-    def __init__(self, points_on_line=[], name=None):
+    def __init__(self, points_on_line={}):
         self.points = points_on_line
-        self.name = name
 
 
 class Angle:
     def __init__(self, line_one, line_two, size=None, relations={}):
-        self.lines = {line_one, line_two}
+        self.lines = [line_one, line_two]
         self.size = size
         self.relations = relations
 
@@ -31,117 +31,144 @@ class Segment:
         self.relations = relations
 
 
+class Polygon:
+    def __init__(self, vertices, convex=True):
+        self.points = vertices
+        self.convex = convex
+        # ?добавить точки внутри?
+
+
 points = []
 lines = []
 angles = []
 segments = []
+polygons = []
 
 figure_types = {'параллелограмм', 'четырехугольник', 'прямоугольник', 'квадрат', 'треугольник',
                 'равносторонний треугольник', 'равнобедренный треугольник', 'трапеция', 'ромб'}
 
 
-def figure_analysis(text):
-    # figure_description_example = "ABCD - квадрат"
-    figure_description = text.split()
-    point_names = list(figure_description[0])
-    figure_type = figure_description[2]
+def polygons_create(text):
+    for polygon in text.split():
+        vertices_list = list(polygon)
 
-    for point_name in point_names:
-        points.append(Point(point_name))
+        for vertice in vertices_list:
+            find_point_with_name(vertice)
 
-    for segment_name in list(combinations(points, 2)):
-        segments.append(Segment(segment_name[0], segment_name[1]))
+        polygons.append(Polygon(deepcopy(points)))
 
-    for point_name in list(combinations(points, 2)):
-        lines.append(Line([point_name[0], point_name[1]]))
+        for vertice_index in range(len(vertices_list)):
+            find_segment_with_points(vertices_list[vertice_index-1], vertices_list[vertice_index])
 
-    for line_name in list(combinations(lines, 2)):
-        angles.append(Angle(line_name[0], line_name[1]))
 
-    # вот здесь генерируются по точкам прямые через них, отрезки и углы между всеми парами прямых
+def segments_create(text):
+    for segment in text.split():
+        seg = Segment(find_point_with_name(segment[0]), find_point_with_name(segment[1]))
+        find_same_segments(segments, seg)
 
-    figure_sides = []
 
-    for i in range(len(point_names)):
-        segment = find_segment_with_points(point_names[i - 1], point_names[i])
-        figure_sides.append(segment)
+def angles_create(text):
+    for angle in text.split(','):
+        angle_name, angle_size = angle.split()
+        A, B, C = list(angle_name)
+        if A != B != C != A:
+            l1 = find_line_with_points(A, B)
+            l2 = find_line_with_points(B, C)
+            setattr(find_angle_with_lines(l1, l2), 'size', int(angle_size))
 
-    # формируем массив всех сторон фигуры
 
-    if len(point_names) == 4:
-        if figure_type == 'квадрат':
+def segments_relations_create(text):
+    for relation in text.split(','):
+        if len(relation.split()[0]) == len(relation.split()[1]) == 2:
+            seg_1 = find_segment_with_points(relation.split()[0][0], relation.split()[0][1])
+            seg_2 = find_segment_with_points(relation.split()[1][0], relation.split()[1][1])
+        if len(relation.split()[0]) == 1:
+            seg_1 = find_segment_with_points(relation.split()[0], relation.split()[1][0])
+            seg_2 = find_segment_with_points(relation.split()[0], relation.split()[1][1])
+        if len(relation.split()[1]) == 1:
+            seg_1 = find_segment_with_points(relation.split()[1], relation.split()[0][0])
+            seg_2 = find_segment_with_points(relation.split()[1], relation.split()[0][1])
 
-            for side_1 in figure_sides:
-                for side_2 in figure_sides:
-                    if get_points_names_from_list(side_1.points) != get_points_names_from_list(side_2.points):
-                        side_1_copy = deepcopy(side_1.relations)
-                        side_1_copy.setdefault(side_2, 1)
-                        setattr(side_1, 'relations', side_1_copy)
+        rel = relation.split()[2]
+        new_relations = deepcopy(seg_1.relations)
+        if seg_2 in new_relations:
+            new_relations[seg_2] = Fraction(rel)
+        else:
+            new_relations.setdefault(seg_2, Fraction(rel))
+        setattr(seg_1, 'relations', new_relations)
 
-            # говорим, что каждый отрезок относится как 1 к 1 с остальными
+        new_relations = deepcopy(seg_2.relations)
+        if seg_1 in new_relations:
+            new_relations[seg_1] = 1/Fraction(rel)
+        else:
+            new_relations.setdefault(seg_1, 1/Fraction(rel))
+        setattr(seg_2, 'relations', new_relations)
 
-            for i in range(len(figure_sides)):
-                line_through_side_1 = find_line_with_segment(figure_sides[i-1])
-                line_through_side_2 = find_line_with_segment(figure_sides[i])
-                angle = find_angle_with_lines(line_through_side_1, line_through_side_2)
-                angle.size = 90
 
-            # говорим, что углы между соседними прямыми, проходящими через отрезки, по 90
+def angles_relations_create(text):
+    for relation in text.split(','):
+        ang_1, ang_2, rel = relation.split()
+        ang_1 = find_angle_with_points(ang_1[0], ang_1[1], ang_1[2])
+        ang_2 = find_angle_with_points(ang_2[0], ang_2[1], ang_2[2])
 
-        if figure_type == 'прямоугольник':
+        new_relations = deepcopy(ang_1.relations)
+        if ang_2 in new_relations:
+            new_relations[ang_2] = Fraction(rel)
+        else:
+            new_relations.setdefault(ang_2, Fraction(rel))
+        setattr(ang_1, 'relations', new_relations)
 
-            figure_sides[0].relations.setdefault(figure_sides[2], 1)
-            figure_sides[2].relations.setdefault(figure_sides[0], 1)
-            figure_sides[1].relations.setdefault(figure_sides[3], 1)
-            figure_sides[3].relations.setdefault(figure_sides[1], 1)
+        new_relations = deepcopy(ang_2.relations)
+        if ang_1 in new_relations:
+            new_relations[ang_1] = 1/Fraction(rel)
+        else:
+            new_relations.setdefault(ang_1, 1/Fraction(rel))
+        setattr(ang_2, 'relations', new_relations)
 
-            # говорим, что противоположные отрезки относятся как 1 к 1 с остальными
 
-            for i in range(len(figure_sides)):
-                line_through_side_1 = find_line_with_segment(figure_sides[i-1])
-                line_through_side_2 = find_line_with_segment(figure_sides[i])
-                angle = find_angle_with_lines(line_through_side_1, line_through_side_2)
-                angle.size = 90
+def line_intersection_create(text):
+    for intersection in text.split(','):
+        l1, l2, P = intersection.split()
 
-        if figure_type == 'ромб':
-            pass
+        l1 = find_line_with_segment(find_segment_with_points(l1[0], l1[1]))
+        new_points = deepcopy(l1.points)
+        new_points.add(find_point_with_name(P))
+        setattr(l1, 'points', new_points)
 
-        if figure_type == 'трапеция':
-            pass
-
-        if figure_type == 'дельтоид':
-            pass
-
-        if figure_type == 'параллелограмм':
-            pass
-
-            # говорим, что углы между соседними прямыми, проходящими через отрезки, по 90
+        l2 = find_line_with_segment(find_segment_with_points(l2[0], l2[1]))
+        new_points = deepcopy(l2.points)
+        new_points.add(find_point_with_name(P))
+        setattr(l2, 'points', new_points)
 
 
 # вспомогательная функция для поиска отрезка по вершинам, указываются имена точек
-def find_segment_with_points(a, b):
-    A = find_point_with_name(a)
-    B = find_point_with_name(b)
+def find_segment_with_points(A, B):
+    a = find_point_with_name(A)
+    b = find_point_with_name(B)
 
-    for segment in segments:
-        if {A, B} == segment.points:
-            return segment
+    for seg in segments:
+        if {a, b} == seg.points:
+            return seg
 
     else:
-        return None
+        new_segment = Segment(a, b)
+        segments.append(new_segment)
+        return new_segment
 
 
-# вспомогательная функция для поиска прямой по точкам на ней, указываются именя точек
-def find_line_with_points(a, b):
-    A = find_point_with_name(a)
-    B = find_point_with_name(b)
+# вспомогательная функция для поиска прямой по точкам на ней, указываются имена точек
+def find_line_with_points(A, B):
+    a = find_point_with_name(A)
+    b = find_point_with_name(B)
 
     for line in lines:
-        if {A, B} <= set(line.points):
+        if {a, b} <= set(line.points):
             return line
 
     else:
-        return None
+        new_line = Line({a, b})
+        lines.append(new_line)
+        return new_line
 
 
 # вспомогательная функция для поиска прямой по отрезку на ней
@@ -153,11 +180,28 @@ def find_line_with_segment(seg):
 # вспомогательная функция для поиска угла по прямым, его задающим
 def find_angle_with_lines(l1, l2):
     for angle in angles:
-        if {l1, l2} == angle.lines:
+        if [set(get_points_names_from_list(l1.points)), set(get_points_names_from_list(l2.points))] == [set(get_points_names_from_list(angle.lines[0].points)), set(get_points_names_from_list(angle.lines[1].points))]:
             return angle
 
     else:
-        return None
+        new_angle = Angle(l1, l2)
+        angles.append(new_angle)
+        return new_angle
+
+
+# вспомогательная функция для поиска угла по точкам
+def find_angle_with_points(A, B, C):
+    l1 = find_line_with_points(A, B)
+    l2 = find_line_with_points(B, C)
+
+    for angle in angles:
+        if angle.lines == [l1, l2]:
+            return angle
+
+    else:
+        new_angle = Angle(l1, l2)
+        angles.append(new_angle)
+        return new_angle
 
 
 # вспомогательная функция для поиска точки по её имени
@@ -167,23 +211,53 @@ def find_point_with_name(A):
             return point
 
     else:
-        return None
+        new_point = Point(A)
+        points.append(new_point)
+        return new_point
 
 
 # вспомогательная функция для получения списка имен точек из списка
 def get_points_names_from_list(points_list):
-    ret = set()
+    ret = []
     for point in points_list:
-        ret.add(point.name)
+        ret.append(point.name)
 
     return ret
 
 
+def find_same_lines(lines_list, line):
+    for lin in lines_list:
+        if len(lin.points & line.points) >= 2:
+            setattr(lin, 'points', lin.points | line.points)
+
+    else:
+        lines_list.append(line)
+
+
+def find_same_segments(segments_list, segment):
+    for seg in segments_list:
+        if seg.points == segment.points:
+            break
+
+    else:
+        segments_list.append(segment)
+
+
+def find_same_angles(angles_list, angle):
+    for ang in angles_list:
+        if ang.lines == angle.lines:
+            break
+
+    else:
+        angles_list.append(angle)
+
+
+# модуль обработки данных при создании json
 # вспомогательная функция для получения номеров объектов списка точек
 def points_processing(points_list):
     ret = []
     for point in points_list:
-        ret.append(f"object_{points.index(point)+1}")
+        ret.append(f"object_{get_points_names_from_list(points).index(point.name)+1}")
 
     return ret
 
@@ -197,29 +271,25 @@ def lines_processing(lines_list):
     return ret
 
 
-# вспомогательная функция для поиска индекса отрезка в списке
-def find_segment_index_in_list(segment):
-    for i in range(len(segments)):
-        if get_points_names_from_list(segments[i].points) == get_points_names_from_list(segment.points):
-            return i
-
-    else:
-        return None
-
-
-def relations_processing(relations_set, par):
+def relations_processing(relations_set, parametr):
     ret = {}
     for object in relations_set:
-        if par == "angle":
+        if parametr == "angle":
             ret.setdefault(f"object_{angles.index(object)+len(points)+len(lines)+1}", relations_set[object])
-        if par == "segment":
-            ret.setdefault(f"object_{find_segment_index_in_list(object)+len(points)+len(lines)+len(angles)+1}", relations_set[object])
+        if parametr == "segment":
+            ret.setdefault(f"object_{relation_processing_helper(object)+len(points)+len(lines)+len(angles)+1}", relations_set[object])
 
     return ret
 
 
+def relation_processing_helper(segment):
+    for i in range(len(segments)):
+        if set(get_points_names_from_list(segments[i].points)) == set(get_points_names_from_list(segment.points)):
+            return i
+
+
 # создание выходной json-ки
-def create_output():
+def json_create():
     output = {}
     object_index = 1
 
@@ -230,10 +300,10 @@ def create_output():
         object.setdefault('type', 'point')
         object.setdefault('points_on_line', None)
         object.setdefault('points_on_segment', None)
-        object.setdefault('cords', point.cords)
         object.setdefault('angle_between_lines', None)
         object.setdefault('size', None)
         object.setdefault('relations', None)
+        object.setdefault('convex', None)
 
         output.setdefault(f"object_{object_index}", object)
         object_index += 1
@@ -241,14 +311,14 @@ def create_output():
     for line in lines:
         object = {}
 
-        object.setdefault('name', line.name)
+        object.setdefault('name', None)
         object.setdefault('type', 'line')
         object.setdefault('points_on_line', points_processing(line.points))
         object.setdefault('points_on_segment', None)
-        object.setdefault('cords', None)
         object.setdefault('angle_between_lines', None)
         object.setdefault('size', None)
         object.setdefault('relations', None)
+        object.setdefault('convex', None)
 
         output.setdefault(f"object_{object_index}", object)
         object_index += 1
@@ -260,10 +330,10 @@ def create_output():
         object.setdefault('type', 'angle')
         object.setdefault('points_on_line', None)
         object.setdefault('points_on_segment', None)
-        object.setdefault('cords', None)
         object.setdefault('angle_between_lines', lines_processing(angle.lines))
         object.setdefault('size', angle.size)
         object.setdefault('relations', relations_processing(angle.relations, 'angle'))
+        object.setdefault('convex', None)
 
         output.setdefault(f"object_{object_index}", object)
         object_index += 1
@@ -275,24 +345,30 @@ def create_output():
         object.setdefault('type', 'segment')
         object.setdefault('points_on_line', None)
         object.setdefault('points_on_segment', points_processing(segment.points))
-        object.setdefault('cords', None)
         object.setdefault('angle_between_lines', None)
         object.setdefault('size', segment.size)
         object.setdefault('relations', relations_processing(segment.relations, 'segment'))
+        object.setdefault('convex', None)
+
+        output.setdefault(f"object_{object_index}", object)
+        object_index += 1
+
+    for polygon in polygons:
+        object = {}
+
+        object.setdefault('name', None)
+        object.setdefault('type', 'segment')
+        object.setdefault('points_on_line', None)
+        object.setdefault('points_on_segment', points_processing(polygon.points))
+        object.setdefault('angle_between_lines', None)
+        object.setdefault('size', None)
+        object.setdefault('relations', None)
+        object.setdefault('convex', polygon.convex)
 
         output.setdefault(f"object_{object_index}", object)
         object_index += 1
 
     return output
-
-
-def find_same_lines():
-    for i in range(len(lines)-1):
-        l1_points = get_points_names_from_list(lines[i].points)
-        for j in range(i+1, len(lines)):
-            l2_points = get_points_names_from_list(lines[j].points)
-            if len(l1_points & l2_points) >= 2:
-                pass
 
 
 json_example = \
@@ -303,17 +379,32 @@ json_example = \
         # only lines
         'points_on_segment': {'A', 'B'},
         # only segments
-        'cords': None,
-        # only points
         'angle_between_lines': {},
         # only angles
         'size': 7,
         # only segments and angles
-        'relations': {'object_name': Fraction(1, 3)}
+        'relations': {'object_name': Fraction(1, 3)},
         # only segments
+        'convex': None
+        # only polygons
     }
 
-text = 'ABCD - квадрат'
-figure_analysis(text)
 
-pprint(create_output())
+# квадрат
+text1 = 'ABCD O' # вводится список многоугольников и отдельных точек
+text2 = 'AC' # вводятся дополнительные отрезки, которые необходимо провести
+text3 = 'ABC 90, BCD 90, CDA 90' # вводится список углов по буквам, начиная "слева"
+text4 = 'AB BC 1/1, CD BC 1/1, CD AD 1/1, AC O 1/1' # вводится список пар отрезок/отрезок,
+# либо отрезок/точка, точка/отрезок с отношениями, причем во вторых двух случаях отношение пишется "слева направо",
+# то есть в примере 'AC O 1/2', AO к CO =  1 к 2
+text5 = 'ABC ABO 2/1' # вводится список отношений углов, порядок точек в угле так же важен
+text6 = 'AC BD O' # вводится по очереди пара отрезков, задающих прямые, и точка пересечения этих прямых
+
+polygons_create(text1)
+segments_create(text2)
+angles_create(text3)
+segments_relations_create(text4)
+angles_relations_create(text5)
+line_intersection_create(text6)
+
+pprint(json_create())
