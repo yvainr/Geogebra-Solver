7135cfea7a268dc043803b88753a8892abccafae
Euggenius <108299501+Euggenius@users.noreply.github.com>
2022-07-09 17:20:15 +0300

    Update polygon_drawer.py

--- polygon_drawer.py
+++ polygon_drawer.py
@@ -10,21 +10,21 @@ def triangle_from_task_drawer(A, B, C):
     BC = taskp.find_segment_with_points(B, C).size
     CA = taskp.find_segment_with_points(C, A).size
     
-    sides = [AB, BC, CA]
+    sides = [BC, CA, AB]
     
-    ABC = taskp.find_angle_with_points(A, B, C).size
-    BCA = taskp.find_angle_with_points(B, C, A).size
-    CAB = taskp.find_angle_with_points(C, A, B).size
+    CBA = taskp.find_angle_with_points(C, B, A).size
+    ACB = taskp.find_angle_with_points(A, C, B).size
+    BAC = taskp.find_angle_with_points(B, A, C).size
     
-    angles = [ABC, BCA, CAB]
+    angles, angles_names = [BAC, CBA, ACB], [f"{B}{A}{C}", f"{C}{B}{A}", f"{A}{C}{B}"]
     
-    triangle_drawer.DrawTriangle(triangle_drawer.CreateTriangle(sides, angles), A, B, C)
+    triangle_drawer.DrawTriangle(triangle_drawer.CreateTriangle(sides, angles, angles_names))
     
     
 # Волчкевич страница 26 задача 1
 text1 = 'ABC'  # многоугольники
-text2 = 'AB 6, BC 4'  # дополнительные отрезки
-text3 = 'CAB 30'  # углы по трем точкам или между прямыми
+text2 = 'AC 4'  # дополнительные отрезки
+text3 = ''  # углы по трем точкам или между прямыми
 text4 = ''  # отношения отрезков
 text5 = ''  # отношения углов
 text6 = ''  # точки пересечения прямых
