ed70a57358ba90f47d3be2e2a150341d34923c1b
Kat <108299258+mathematessa@users.noreply.github.com>
2022-07-13 02:18:22 +0300

    new version, but still incomplete

--- language_interpreter.py
+++ language_interpreter.py
@@ -1,64 +1,50 @@
 output_num = 6 
 #number of output strings
 
-extra_symbols = {33, 35, 36, 37, 38, 40, 41, 42, 43, 45, 46, 47, 58, 60, 61, 62, 63, 64, 92, 93, 94, 95, 96, 123, 124, 125, 126}
-#ord of extra symbols
+def main_func(inp_str):
+    #main func
+    inp_str = string_split(inp_str)
+    ret = assign_to_classes(inp_str)
+    return ret #, sep="\n"
 
-def extra_symbols_deleting(inp_str):
-    input_str = ""
-    for i in range(len(inp_str)):
-        try:
-            if ord(inp_str[i]) not in extra_symbols:
-                input_str = input_str[:] + str(inp_str[i])            
-        except:
-            pass
-    return input_str
-
-def input_str(inp_str):
-    #func for input split
-    inp_str = inp_str.split(';')
+def string_split(inp_str):
+    #creating list with diff objects
+    inp_str = inp_str.split(",")
     return inp_str
 
-
-def print_ret(inp_str):
-    #func for final output
-    # print(inp_str)
-    for i in range(output_num):
-        if ord(inp_str[i][0]) == 32:
-            inp_str[i] = inp_str[i][1:]
-        print(inp_str[i])
-
-
-def spaces_normalize(inp_str):
-    #func for deleting extra spaces
-    for i in range(len(inp_str) - 2):
+def distillation(part):
+    #highlithing the parts for final output
+    new_part = ""
+    for i in range(len(part)):
         try:
-            if ord(inp_str[i]) == 32:
-                if ord(inp_str[i+1]) == 32 or ord(inp_str[i+1]) == 44:
-                    inp_str = inp_str[:i+1] + inp_str[i+2:]
+            if (47 < ord(part[i])< 58) or (64 < ord(part[i]) < 91) or (96 < ord(part[i]) < 123) or ord(part[i]) == 32:
+                new_part = new_part[:] + str(part[i])            
         except:
             pass
-    return inp_str
+    return new_part.strip()
 
+def assign_to_classes(inp_str):
+    polygons = []
+    segments = []
+    angles = []
+    segments_relations = []
+    angles_relations = []
+    lines_intersection = []
+    for part in inp_str:
+        if part.find("угольн") != -1:
+            polygons.append(distillation(part))
+        elif part.find("отрез") + part.find("сторон") + part.find("диагонал") > -3:
+            segments.append(distillation(part))
+        elif part.find("отно") != -1 and part.find("отр") != -1:
+            segments_relations.append(distillation(part))
+        elif part.find("отно") != -1 and part.find("уг") != -1:
+            angles_relations.append(distillation(part))
+        elif part.find("перес") != -1:
+            lines_intersection.append(distillation(part))
+        elif part.find("уг") != -1:
+            angles.append(distillation(part))
+    return polygons, segments, angles, segments_relations, angles_relations, lines_intersection
 
-def comma_normalize(inp_str):   
-    #func for deleting extra commas
-    for i in range(len(inp_str) - 1):
-        try:
-            if ord(inp_str[i]) == 44:
-                if ord(inp_str[i+1]) == 44:
-                    inp_str = inp_str[:i] + inp_str[i+1:]
-                if ord(inp_str[i+1]) == 32 and (ord(inp_str[i+2]) == 44 or ord(inp_str[i+2]) == 32):
-                    inp_str = inp_str[:i+1] + inp_str[i+2:]
-        except:
-            pass
-    return inp_str
+inp = "треугольник ABC, угол ABC равен 30, сторона BC равна 10"
+print(main_func(inp))
 
-
-inp = "ABC    ; BD,, % ***** ,CD ; ABC 30 ; '' ; } ]]'' ; '' "
-
-inp = extra_symbols_deleting(inp)
-inp = spaces_normalize(inp)
-inp = comma_normalize(inp)
-inp = input_str(inp)
-print_ret(inp)
