280cc88509782e3a3d41f40bcad3f5a71e828f6b
Kat <108299258+mathematessa@users.noreply.github.com>
2022-07-14 20:54:50 +0300

    improved appearance, added the ability to input nonintegers

--- language_interpreter.py
+++ language_interpreter.py
@@ -1,8 +1,7 @@
-output_num = 7 
-#number of output strings
+#number of output strings = 7
 
-def main_func(inp_str):
-    #main func
+def text_analyze(inp_str, output_num=6):
+    """main func"""
     inp_str = string_split(inp_str)
     ret = assign_to_classes(inp_str)
     ret = list(ret)
@@ -15,33 +14,37 @@ def main_func(inp_str):
         #     ret[i] = ret[i][:] + "\n"
     return output_text #, sep="\n"
 
+
 def string_split(inp_str):
-    #creating list with diff objects
+    """creating list with diff objects"""
     inp_str = inp_str.split(",")
     return inp_str
 
+
 def distillation(part):
-    #highlithing the parts for final output
+    """highlithing the parts for final output"""
     new_part = ""
     for i in range(len(part)):
         try:
-            if (46 < ord(part[i])< 58) or (64 < ord(part[i]) < 91) or (96 < ord(part[i]) < 123) or ord(part[i]) == 32:
+            if (45 < ord(part[i])< 58) or (64 < ord(part[i]) < 91) or (96 < ord(part[i]) < 123) or ord(part[i]) == 32:
                 new_part = new_part[:] + str(part[i])            
         except:
             pass
     return new_part.strip()
 
+
 def remarkable_point_processing(part):
-    #smth for points
-    if part.find("опис") != -1:
+    """smth for points"""
+    if "опис" in part:
         part = part.replace("опис", "O")
-    elif part.find("медиан") != -1:
+    elif "медиан" in part:
         part = part.replace("медиан", "M")
-    elif part.find("бисс") != -1:
+    elif "бисс" in part:
         part = part.replace("бисс", "I")
-    elif part.find("высот") != -1:
+    elif "высот" in part:
         part = part.replace("высот", "H")
     return part
+
 
 def assign_to_classes(inp_str):
     polygons = []
@@ -50,25 +53,25 @@ def assign_to_classes(inp_str):
     segments_relations = []
     angles_relations = []
     lines_intersection = []
-    remarkable_points = []
-    for part in inp_str:
-        if part.find("угольн") != -1:
-            polygons.append(distillation(part))
-        elif part.find("отно") != -1 and (part.find("отр") != -1 or part.find("сторон") != -1):
-            segments_relations.append(distillation(part))
-        elif part.find("отно") != -1 and part.find("уг") != -1:
-            angles_relations.append(distillation(part))
-        elif part.find("отрез") != -1 or part.find("сторон") != -1 or part.find("диагонал") != -1:
-            segments.append(distillation(part))
-        elif part.find("перес") != -1:
-            lines_intersection.append(distillation(part))
-        elif part.find("уг") != -1:
-            angles.append(distillation(part))
-        elif part.find("опис") or (part.find("перес") and (part.find("медиан") != 3 or part.find("бисс") != -1 or part.find("высот") != -1)):
-            part = remarkable_point_processing(part)
-            remarkable_points.append(distillation(part))
-    return polygons, segments, angles, segments_relations, angles_relations, lines_intersection, remarkable_points
 
-inp = "треугольник ABC, угол ABC равен 30, сторона BC равна 10, угол BCA равен 40, сторона AB относится к BC как 5/3, S точка прересечения медиан ABC"
-print(main_func(inp))
+    for part in inp_str:
+        if "угольн" in part:
+            polygons.append(distillation(part))
+        elif "отно" in part and ("отр" in part or "сторон" in part):
+            segments_relations.append(distillation(part))
+        elif "отно" in part and "уг" in part:
+            angles_relations.append(distillation(part))
+        elif "опис" in part or (("перес" in part  and (("медиан" in part or "бисс" in part or "высот" in part)))):
+            part = remarkable_point_processing(part)
+            polygons.append(distillation(part))
+        elif "отрез" in part or "сторон" in part or "диагонал" in part:
+            segments.append(distillation(part))
+        elif "перес" in part:
+            lines_intersection.append(distillation(part))
+        elif "уг" in part:
+            angles.append(distillation(part))
+    return polygons, segments, angles, segments_relations, angles_relations, lines_intersection
+
+inp = 'треугольник ABC, сторона AB = 5, сторона BC = 4, сторона AB относится к AC как 5/3, угол ACB равен 90, I точка пересечения биссектрисс в ABC'
+print(text_analyze(inp))
 
