0de88be3eef542dd6bcf81dc32a0bb8be166a8b5
Kat <108299258+mathematessa@users.noreply.github.com>
2022-07-24 23:11:26 +0300

    added algebraic sum of elements (task+question)

--- language_interpreter.py
+++ language_interpreter.py
@@ -1,18 +1,21 @@
 from re import *
 #number of output strings = 8
 
-def text_analyze(inp_str, output_num=8):
+def text_analyze(inp_str, output_num=7):
     """main function"""
     inp_str = inp_str.split("\n")
     statement = inp_str[0]
-    try:
+    question_existence = len(inp_str) > 1 # check question existence
+    if question_existence: # creat question if exist
         question = inp_str[1]
-    except:
-        question = ""
+
     statement = string_split(statement)
     ret = new_assign_to_classes(statement)
     ret = list(ret)
-    ret.append(question_processing(question))
+
+    if question_existence: # analyze question if exist
+        ret.append(question_processing(question))
+
     for i in range(output_num):
         ret[i] = str(ret[i])
     output_text = ''
@@ -47,12 +50,19 @@ def output_formatting(part):
     return part
 
 
+def summ_formatting(part):
+    """new format for task_parser"""
+    part = sub(r"([+-])([A-Z]+)", r"\2\1", part)
+    return part
+
+
 def distillation(part):
     """highlithing the parts for final output"""
+    part = sub(r' [-]+ ', ' ', part)
     new_part = ""
     for i in range(len(part)):
         try:
-            if ord(part[i]) == 42 or (45 < ord(part[i])< 58) or (64 < ord(part[i]) < 91) or ord(part[i]) == 32:
+            if ord(part[i]) == 42 or ord(part[i]) == 43 or (44 < ord(part[i])< 58) or (64 < ord(part[i]) < 91) or ord(part[i]) == 32:
                 new_part = new_part[:] + str(part[i])
         except:
             pass
@@ -68,20 +78,41 @@ def equality_of_elem(part):
     return part
 
 
+def algebraic_sum(part):
+    """formatting sum and diff of angles and segments"""
+    if "больш" in part:
+        part = distillation(part)
+        part = sub(r"(^[A-Z][A-Z])(\s)([A-Z][A-Z])", r"-\1 \3 ", part)
+        part = sub(r"(^[A-Z][A-Z][A-Z])(\s)([A-Z][A-Z][A-Z])", r"-\1 \3 ", part)
+    elif "меньш" in part:
+        part = distillation(part)
+        part = sub(r"(^[A-Z][A-Z])(\s)([A-Z][A-Z])", r"-\3 \1 ", part)
+        part = sub(r"(^[A-Z][A-Z][A-Z])(\s)([A-Z][A-Z][A-Z])", r"-\3 \1 ", part)
+    elif "сумм" in part:
+        part = distillation(part)
+        part = sub(r"(^[A-Z][A-Z])(\s)([A-Z][A-Z])", r"+\1 \3 ", part)
+        part = sub(r"(^[A-Z][A-Z][A-Z])(\s)([A-Z][A-Z][A-Z])", r"+\1 \3 ", part)
+    return part
+
+
 def remarkable_point_processing(part):
-    """smth for points"""
+    """remarkable points parcing"""
     if "опис" in part:
-        part = part.replace("опис", "O")
+        part = sub("опис", "O", part)
+        # part = part.replace("опис", "O")
     elif "медиан" in part or "центроид" in part:
-        part = sub("центроид", "медиан", part)
-        part = part.replace("медиан", "M")
+        part = sub("центроид", "M", part)
+        part = sub("медиан", "M", part)
+        # part = part.replace("медиан", "M")
     elif "бисс" in part or "инцентр" in part or "впис" in part:
-        part = sub("инцентр", "бисс", part)
-        part = sub("впис", "бисс", part)
-        part = part.replace("бисс", "I")
+        part = sub("инцентр", "I", part)
+        part = sub("впис", "I", part)
+        part = sub("бисс", "I", part)
+        # part = part.replace("бисс", "I")
     elif "высот" in part or "ортоцен" in part:
-        part = sub("ортоцен", "высот", part)
-        part = part.replace("высот", "H")
+        part = sub("ортоцен", "H", part)
+        part = sub("высот", "H", part)
+        # part = part.replace("высот", "H")
     return part
 
 
@@ -91,6 +122,9 @@ def class_analyze(part):
     poly = False
     if "угольн" in part or "подоб" in part:
         poly = True
+
+    if "больш" in part or "меньш" in part or "сумм" in part:
+        part = algebraic_sum(part)
 
     part = equality_of_elem(part)
     part = part.split()
@@ -106,7 +140,12 @@ def class_analyze(part):
                 segments_parts += 1
             elif len(object) > 2:
                 let_parts += 1
-        elif 45 < ord(object[0]) < 59:
+        elif ord(object[0]) == 43 or  ord(object[0]) == 45:
+            if len(object) == 3:
+                segments_parts += 1
+            elif len(object) == 4:
+                let_parts += 1
+        elif 44 < ord(object[0]) < 59:
             num_parts += 1
 
     ret = "polygon"
@@ -144,13 +183,16 @@ def new_assign_to_classes(inp_str):
 
     for part in inp_str:
         cla = class_analyze(part)
+        part = algebraic_sum(part)
         part = equality_of_elem(part)
         if cla == "polygon":
             part = sub("невыпукл", "*", part)
             polygons.append(distillation(part))
         elif cla == "segment_rel":
+            part = summ_formatting(part)
             segments_relations.append(distillation(part))
         elif cla == "angle_rel":
+            part = summ_formatting(part)
             angles_relations.append(distillation(part))
         elif cla == "rem_point":
             part = remarkable_point_processing(part)
@@ -164,6 +206,7 @@ def new_assign_to_classes(inp_str):
         elif cla == "polygon_rel":
             polygon_relation.append(distillation(part))
 
+    # print(segments_relations)
     return polygons, segments, angles, segments_relations, angles_relations, polygon_relation, lines_intersection
 
 
@@ -173,18 +216,23 @@ def question_processing(question):
     if "угольн" in question or "подоб" in question:
         poly = True
     question = equality_of_elem(question)
-    question = distillation(question)
     if poly:
+        question = distillation(question)
         question = sub(r"(^\w+)(\s)(\w*)(\s)(\d)", r'/\1 \3 \5', question)
         question = sub(r"(^\w+)(\s)(\w*$)", r'/\1 \3 ?', question)
+    elif "больш" in question or "меньш" in question or "сумм" in question:
+        question = algebraic_sum(question)
+        print(question)
+        question = sub(r"([A-Z])(\s*)$", r"\1 ?", question)
+        question = summ_formatting(question)
     else:
+        question = distillation(question)
         question = sub(r"(^\w+)(\s)(\w*)(\s)(\d+)", r'\1 \3 \5', question)
         question = sub(r"(^\w+)(\s)(\w*$)", r'\1 \3 ?', question)
     return question
 
 
-inpu = 'ABC, DEF, ABC подобен DEF с коэффициентом 1/2, relation of sides AB and BC is 1/2'
-inp = 'треугольники ABC = HFG, ABCD невыпуклый, AB = 5, сторона BC равна 4, AB = AC,  ACB равен 90, I инцентр в ABC, точкой M сторона AB делится в отношении 1/2 \n доказать что подобны ABC DEF 1/2'
-print(text_analyze(inp))
-
+# inpu = 'ABC, DEF, ABC подобен DEF с коэффициентом 1/2, relation of sides AB and DE is 1/2 \n prove that BC relate EF like 1/2'
+# inp = 'треугольники ABC = HFG, ABCD невыпуклый, AB = 5, сторона BC равна 4, AB = AC, angle RUS меньше и UTS стороны на 5, ACB равен 90, I -- инцентр в ABC, точкой M сторона AB делится в отношении 1/2 \n найти сумму ABC DEF'
+# print(text_analyze(inp))
 
