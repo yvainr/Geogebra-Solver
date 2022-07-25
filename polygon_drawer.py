2bea760684a2d6009642df43c8444b906dc200d7
Euggenius <108299501+Euggenius@users.noreply.github.com>
2022-07-25 23:53:26 +0300

    Update polygon_drawer.py

--- polygon_drawer.py
+++ polygon_drawer.py
@@ -1,12 +1,11 @@
-import drawer_ggb.geogebra_html_generator as geogebra_html_generator
+import geogebra_html_generator
 import triangle_drawer as triad
 import task_parser as taskp
 from math import *
+from itertools import combinations
 
 
-# отрисовка треугольника по именам вершин
-def triangle_from_task_drawer(A, B, C):
-
+def get_triangle_parameter(A, B, C):
     AB = taskp.find_segment_with_points(A, B).size
     BC = taskp.find_segment_with_points(B, C).size
     CA = taskp.find_segment_with_points(C, A).size
@@ -17,36 +16,29 @@ def triangle_from_task_drawer(A, B, C):
     ACB = taskp.find_angle_with_points(A, C, B).size
     BAC = taskp.find_angle_with_points(B, A, C).size
 
-    if not CBA:
-        try:
-            CBA = 180 - taskp.find_angle_with_points(A, B, C).size
-        except TypeError:
-            pass
-    if not ACB:
-        try:
-            ACB = 180 - taskp.find_angle_with_points(B, C, A).size
-        except TypeError:
-            pass
-    if not BAC:
-        try:
-            BAC = 180 - taskp.find_angle_with_points(C, A, B).size
-        except TypeError:
-            pass
-
     angles, angles_names = [BAC, CBA, ACB], [f"{B}{A}{C}", f"{C}{B}{A}", f"{A}{C}{B}"]
 
-    return triad.CreateTriangle(sides, angles, angles_names, sides_names)
+    return sides, angles, angles_names, sides_names
 
 
-def draw_polygon(points):
-	ans = list()
-	ans_polygon = 'Polygon('
-	for point in points:
-	    ans.append(f"{point.name}({triad.Equal(point.x)}, {triad.Equal(point.y)})")
-	    ans_polygon += f"{point.name}, "
-	ans_polygon = ans_polygon[:-2] + ')'
-	ans.append(ans_polygon)
-	geogebra_html_generator.insert_commands(ans)
+def draw_polygon(points, realize_data):
+    ans_polygon = 'Polygon('
+    for point in points:
+        realize_data.append(f"{point.name}({triad.Equal(point.x)}, {triad.Equal(point.y)})")
+        ans_polygon += f"{point.name}, "
+    ans_polygon = ans_polygon[:-2] + ')'
+    realize_data.append(ans_polygon)
+
+
+def draw_specific_points(realize_data):
+    for point in taskp.points:
+        if point.specific_properties_point:
+            A = taskp.find_point_with_name(point.specific_properties_triangle[0])
+            B = taskp.find_point_with_name(point.specific_properties_triangle[1])
+            C = taskp.find_point_with_name(point.specific_properties_triangle[2])
+
+            for draw_data in triad.SpecificPointGeneration(point.name, point.specific_properties_point, A, B, C):
+                realize_data.append(draw_data)
 
 
 def triangle_with_segment_synchronization(A, B, C, P1, P2):
@@ -70,23 +62,66 @@ def triangle_with_segment_synchronization(A, B, C, P1, P2):
     return P1, P2, P3
 
 
-def text_splitter(text):
-    taskp.points.clear()
-    taskp.lines.clear()
-    taskp.angles.clear()
-    taskp.segments.clear()
-    taskp.polygons.clear()
+def draw_lines_intersections(intersections, realize_data):
+    if len(intersections) > 0:
+        for intersection in intersections.split(','):
+            l1 = f"Line({intersection.split()[0][0]}, {intersection.split()[0][1]})"
+            l2 = f"Line({intersection.split()[1][0]}, {intersection.split()[1][1]})"
+            point_name = intersection.split()[2]
+            realize_data.append(f"{point_name}=Intersect({l1}, {l2})")
 
-    text = text.replace('\r', '').split('\n')
 
+def create_polygon(vertices):
+    if len(vertices) == 3:
+        A, B, C = vertices
+
+        return triad.CreateTriangle(*get_triangle_parameter(A, B, C))
+
+    if len(vertices) == 4:
+        perspective_triangle = None
+        perspective_current_size = 0
+        for triangle in combinations(vertices, 3):
+            triangle_parameter = get_triangle_parameter(triangle[0], triangle[1], triangle[2])
+            perspective_size = 6 - triangle_parameter[0].count(None) - triangle_parameter[1].count(None)
+
+            if perspective_size >= perspective_current_size:
+                perspective_triangle = triangle
+
+        A, B, C = triad.CreateTriangle(get_triangle_parameter(perspective_triangle[0], perspective_triangle[1], perspective_triangle[2]))
+
+
+realize_data = list()
+
+# Волчкевич страница 26 задача 1
+text1 = 'ABC, DEF'  # многоугольники
+text2 = 'AB 10'  # дополнительные отрезки
+text3 = ''  # углы по трем точкам или между прямыми
+text4 = ''  # отношения отрезков
+text5 = ''  # отношения углов
+text6 = 'ABC DEF 2'  # отношения многоугольников
+text7 = ''  # точки пересечения прямых
+
+taskp.polygons_create(text1)
+taskp.segments_create(text2)
+taskp.angles_create(text3)
+taskp.segments_relations_create(text4)
+taskp.angles_relations_create(text5)
+taskp.polygons_relations_create(text6)
+taskp.line_intersection_create(text7)
+
+for polygon in taskp.polygons:
     try:
-        taskp.polygons_create(text[0])
-        taskp.segments_create(text[1])
-        taskp.angles_create(text[2])
-        taskp.segments_relations_create(text[3])
-        taskp.angles_relations_create(text[4])
-        taskp.line_intersection_create(text[5])
+        taskp.UseRelations(taskp.segments)
+        taskp.UseRelations(taskp.angles)
+        draw_polygon(create_polygon(taskp.get_points_names_from_list(polygon.points)), realize_data)
     except IndexError:
         pass
 
-    draw_polygon(triangle_from_task_drawer(taskp.polygons[0].points[0].name, taskp.polygons[0].points[1].name, taskp.polygons[0].points[2].name))
+draw_specific_points(realize_data)
+
+try:
+    draw_lines_intersections(text7, realize_data)
+except IndexError:
+    pass
+
+geogebra_html_generator.insert_commands(realize_data)
