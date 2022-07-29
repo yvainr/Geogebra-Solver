60ca3f6566eb9df68ee9a3884515c493b0e9e8bc
Euggenius <108299501+Euggenius@users.noreply.github.com>
2022-07-29 03:39:02 +0300

    Update objects_types.py

--- objects_types.py
+++ objects_types.py
@@ -11,3 +11,26 @@ class Objects:
 
     def __str__(self):
         return f'{self.points}\n{self.lines}\n{self.rays}\n{self.angles}\n{self.segments}\n{self.polygons}\n{self.facts}\n{self.questions}'
+
+
+def create_objects_copy(objects):
+    ret = Objects()
+
+    for point in objects.points:
+        ret.points.append(point)
+    for line in objects.lines:
+        ret.lines.append(line)
+    for ray in objects.rays:
+        ret.rays.append(ray)
+    for angle in objects.angles:
+        ret.angles.append(angle)
+    for segment in objects.segments:
+        ret.segments.append(segment)
+    for polygon in objects.polygons:
+        ret.polygons.append(polygon)
+    for fact in objects.facts:
+        ret.facts.append(fact)
+    for question in objects.questions:
+        ret.questions.append(question)
+
+    return ret
